0c4caeb2c5dafd5a425d1e598f10ac551b849009
akameco <akameco.t@gmail.com>
2019-08-12 22:59:35 +0900

    chore(deps): upgrade deps

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,4 @@
 import { toMatchDiffSnapshot, getSnapshotDiffSerializer } from "snapshot-diff";
-import "@testing-library/react/cleanup-after-each";
 import "@testing-library/jest-dom/extend-expect";
 
 expect.addSnapshotSerializer(getSnapshotDiffSerializer());
